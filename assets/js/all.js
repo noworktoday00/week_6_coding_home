7b82cd04de012d09d0b4e70efa2f352998109e8b
van <noworktoday00@gmail.com>
2022-09-12 09:57:17 +0800

    Update 2022-09-12T01:57:02.449Z

---
+++ assets/js/all.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var swiper = new Swiper(".swiper", {
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true
+  },
+  navigation: {
+    nextEl: ".swiper-arrow-next",
+    prevEl: ".swiper-arrow-prev"
+  },
+  pagination: {
+    el: ".swiper-pagination"
+  },
+  mousewheel: true
+});
+//# sourceMappingURL=all.js.map
