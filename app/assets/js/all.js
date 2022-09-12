511ca819542461d42d29ab0fead448b369ff3813
van <noworktoday00@gmail.com>
2022-09-12 09:55:33 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".swiper", {
+  effect: "fade",
+  fadeEffect:{
+    crossFade: true,
+  },
+  navigation: {
+    nextEl: ".swiper-arrow-next",
+    prevEl: ".swiper-arrow-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel:true,
+});
